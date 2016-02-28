f44aa5d1e6fdc5372bcf3eea3de3d6344c1cc1fe
Marvin Pinto <marvin@pinto.im>
2016-02-27 20:18:40 -0500

    Process all user-supplied directories that begin with MEDIA_ and convert all their mkv files to mp4s

--- Dockerfile
+++ Dockerfile
@@ -4,22 +4,13 @@ ENV DEBIAN_FRONTEND noninteractive
 WORKDIR /
 
 RUN apt-get -qq update \
-  && apt-get install -y software-properties-common \
-  && apt-add-repository -y ppa:mc3man/trusty-media \
-  && apt-get -qq update \
   && apt-get install -y \
-    git \
-    mkvtoolnix \
-    gpac \
-    ffmpeg \
-    python \
+    libav-tools \
+    curl \
   && apt-get clean autoclean \
   && apt-get autoremove -y --purge \
   && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* \
   && rm -rf /var/lib/{apt,dpkg,cache,log}/
 
-RUN git clone https://github.com/gavinbeatty/mkvtomp4.git /app
-RUN cd /app && git checkout -f 99c3ef97cd353b2afa98204a83e74caaab70cb09
-RUN cd /app && python setup.py install
-
 ADD entrypoint.sh .
+CMD ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,35 @@
 #!/bin/bash
 
-# Get all environment variables that begin with "MEDIA_"
-for x in ${!MEDIA_*}; do
-  echo "$x"
+while true; do
+
+  # Loop through each of the directories specified via the `MEDIA_xxxx`
+  # environment variables
+  for x in ${!MEDIA_*}; do
+    media_dir=${!x}
+    find "$media_dir" -type f -name "*.mkv" | while read file; do
+      echo "Now converting: $file"
+      m_filename=`basename "$file"`
+      m_dirname=`dirname "$file"`
+      m_filename_wo_ext="${m_filename%.*}"
+      m_newfilename="${m_dirname}/${m_filename_wo_ext}.mp4"
+      avconv -loglevel error -y -i "$file" -c:v copy -c:a aac -strict experimental "$m_newfilename"
+      if [ "$?" -eq 0 ]; then
+        echo "Successfully processed: $file"
+        /bin/rm -f "$file"
+      fi
+
+      # Pause for 10 seconds before moving on to the next file
+      sleep 10
+
+    done
+  done
+
+  # Trigger a refresh of the Movies & TV Shows sections in plex
+  if [ -n "$PLEX_URL" ]; then
+    curl -s -H "X-Plex-Token: ${PLEX_TOKEN}" http://${PLEX_URL}/library/sections/1/refresh
+    curl -s -H "X-Plex-Token: ${PLEX_TOKEN}" http://${PLEX_URL}/library/sections/2/refresh
+  fi
+
+  echo "Media converter going to sleep for 10 mins"
+  sleep 600
 done
